2a103237386dd70eff39782d35ea4cf81736f685
Taribi-Isaac <taribiforoyou@gmail.com>
2023-08-26 18:25:02 +0100

    files deleted

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 
 export const client = sanityClient({
-  projectId: process.env. REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,10 +1,8 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
+
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
@@ -16,36 +14,28 @@ import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
 
-import profile from '../assets/profile.png';
+
+
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
+
+
+
+
 import zic from '../assets/zic.png';
-import image1 from '../assets/image1.png';
+
 import image from '../assets/image.png';
 
 export default {
 email,
   mobile,
-  api,
-  cpp,
+  
+  
   css,
   figma,
-  flutter,
+ 
   git,
   graphql,
   html,
@@ -57,22 +47,16 @@ email,
   redux,
   sass,
   typescript,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
-  profile,
+  
+  
   circle,
-  logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
+  
+  
+  
+ 
+ 
+  
   zic,
-  image1,
+  
   image,
 };
