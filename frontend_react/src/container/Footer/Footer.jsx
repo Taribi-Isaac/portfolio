d12cbade504be9cf24e35e130430b28bd78af554
Taribi-Isaac <taribiforoyou@gmail.com>
2023-08-30 03:09:32 +0100

    email link updated

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">taribiforyou@gmail.com</a>
+          <a href="mailto:taribiforyou@gmail.com" className="p-text">taribiforyou@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
